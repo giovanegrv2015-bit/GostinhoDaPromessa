8c18a451fffabf10cb85d846ccce20e45bedd684
GIOVANE PEREIRA DOS SANTOS <giovanegrv2015@gmail.com>
2026-05-25 21:29:09 -0300

    continuacao da logica do gerenciamento.sj

--- src/main/webapp/js/gerenciamento.js
+++ src/main/webapp/js/gerenciamento.js
@@ -7,5 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
     carregarProdutos();
 
     document.getElementById("modal-quantidade").addEventListener("input", calcularTottal);
-    
+    document.getElementById("modal-valor").addEventListener("input", calcularTotal);
+
+    document.getElementById("buscarGerenciamento").addEventListener("input", (e) => {
 });
+});
+
+async function carregarProdutos(busca = "") {
+    const container = document.getElementById("listarProdutos");
+    container.innerHTML = '<div class="loading-msg">Carregando produtos...</div';
+
+    try {
+        let url = BASE_URL;
+        if(busca) {
+            url += `?nome=${encodeURIComponent(busca)}`;
+        }
+
+        const response = await fetch(url);
+        if(response.ok) {
+            produtosAtuais = await response.json();
+        } else {
+            container.innerHTML = '<p class="error-msg">Falha de conexão com o servidor.</p>';
+        }
+}
